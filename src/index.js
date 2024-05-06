5c9158034719ba4c1066a73ee964f4f805c0038f
ramsingh-verma369 <ramsinghverma9450@gmail.com>
2024-05-06 17:53:53 +0530

    setting of utils file and middleware

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 // require('dotenv').config({path: './env'});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config();
 
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+.then( () => {
+    app.on('error', (error) => {
+        console.log("ERRR",error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("Mongo db connection is fa;iled",err)
+})
 
 
 
